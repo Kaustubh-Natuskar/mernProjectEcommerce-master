45f9adcc1fb5fe53187e19832c46fa43471e871f
kaustubh <kaustubhnatuskar143@gmail.com>
2022-04-17 11:30:24 +0530

    changed footer, contac,abaout

--- frontend/src/component/layout/About/About.js
+++ frontend/src/component/layout/About/About.js
@@ -2,35 +2,32 @@ import React from "react";
 import "./aboutSection.css";
 import { Button, Typography, Avatar } from "@material-ui/core";
 import YouTubeIcon from "@material-ui/icons/YouTube";
-import InstagramIcon from "@material-ui/icons/Instagram";
+import InstagramIcon from "@material-ui/icons/LinkedIn";
 const About = () => {
   const visitInstagram = () => {
-    window.location = "https://instagram.com/meabhisingh";
+    window.location = "https://www.linkedin.com/in/kaustubhnatuskar/";
   };
   return (
-    <div className="aboutSection">
-      <div></div>
-      <div className="aboutSectionGradient"></div>
-      <div className="aboutSectionContainer">
-        <Typography component="h1">About Us</Typography>
+		<div className="aboutSection">
+			<div></div>
+			<div className="aboutSectionGradient"></div>
+			<div className="aboutSectionContainer">
+				<Typography component="h1">About Us</Typography>
 
-        <div>
-          <div>
-            <Avatar
-              style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
-              src="https://res.cloudinary.com/tripleayt/image/upload/v1631555947/products/jpyibarlaxawvcvqjv5b.png"
-              alt="Founder"
-            />
-            <Typography>Abhishek Singh</Typography>
-            <Button onClick={visitInstagram} color="primary">
-              Visit Instagram
-            </Button>
-            <span>
-              This is a sample wesbite made by @meabhisingh. Only with the
-              purpose to teach MERN Stack on the channel 6 Pack Programmer
-            </span>
-          </div>
-          <div className="aboutSectionContainer2">
+				<div>
+					<div>
+						<Avatar
+							style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
+							src="https://res.cloudinary.com/dingwe4w2/image/upload/v1650174953/COLOR_POP_gjlyz9.jpg"
+							alt="Founder"
+						/>
+						<Typography>Kaustubh Natuskar</Typography>
+						<Button onClick={visitInstagram} color="primary">
+							Visit LinkedIn
+						</Button>
+						<span></span>
+					</div>
+					{/* <div className="aboutSectionContainer2">
             <Typography component="h2">Our Brands</Typography>
             <a
               href="https://www.youtube.com/channel/UCO7afj9AUo0zV69pqEYhcjw"
@@ -42,11 +39,11 @@ const About = () => {
             <a href="https://instagram.com/meabhisingh" target="blank">
               <InstagramIcon className="instagramSvgIcon" />
             </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+          </div> */}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default About;
--- frontend/src/component/layout/Contact/Contact.js
+++ frontend/src/component/layout/Contact/Contact.js
@@ -4,12 +4,12 @@ import { Button } from "@material-ui/core";
 
 const Contact = () => {
   return (
-    <div className="contactContainer">
-      <a className="mailBtn" href="mailto:mymailforabhi@gmail.com">
-        <Button>Contact: mymailforabhi@gmail.com</Button>
-      </a>
-    </div>
-  );
+		<div className="contactContainer">
+			<a className="mailBtn" href="mailto:kaustubhnatuskar@gmail.com">
+				<Button>Contact:kaustubhnatuskar@gmail.com</Button>
+			</a>
+		</div>
+	);
 };
 
 export default Contact;
